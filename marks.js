41fe4380c4008c1a9ac483a19e8b7b6f48ebee32
jui29 <jui29>
2021-03-10 02:59:31 +0600

    first commit

---
+++ marks.js
@@ -0,0 +1,84 @@
+var marks=100;
+var studentSub1=80;
+var studentSub2=75;
+var studentSub3=70;
+var studentSub4=65;
+var studentSub5=60;
+var studentSub6=55;
+var studentSub7=50;
+var studentSub7=45;
+
+if (marks > studentSub1) {
+    console.log("Letter Grade A+  1st class 80+ Above CGPA 4.00")
+}
+
+ else if(marks > studentSub2 ){
+    console.log("Letter Grade A  1st class 75 to less then 80 CGPA 3.75")
+ }
+
+ else if (marks > studentSub3 ){
+    console.log("Letter Grade A-  1st class 70 to less then 75  CGPA 3.75")
+}
+
+else if(marks > studentSub4){
+    console.log("Letter Grade B+ 1st class 65 to less the 70  CGPA 3.25")
+}
+
+else if(marks > studentSub5){
+    console.log("Letter Grade B  1st class 60 to less the 65 CGPA 3.00")
+}
+
+else if(marks > studentSub6){
+    console.log("Letter Grade B- 2nd class 55 to less then 60 CGPA 2.75")
+}
+
+else if(marks > studentSub7){
+    console.log("Letter Grade C+ 2rd  class 45 to less the 55 CGPA 2.50")
+}
+
+
+else{
+    
+    console.log("Letter Grade F 30 to less then 33 CGPA 1.00")
+}
+
+
+
+
+
+//var Scroe=100;
+
+
+//if (Scroe >= 80) {
+    //document.write("A+")
+//}
+
+ //else if(Scroe >= 70){
+    //document.write("A")
+ //}
+
+ //else if (Scroe >= 60 ){
+    //document.write("A-")
+//}
+
+//else if(Scroe >= 50){
+    //document.write ("B")
+//}
+
+//else{
+    
+    //document.write("F")
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
